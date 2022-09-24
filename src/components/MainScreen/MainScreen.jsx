ece8c2ccc40a6532e9c4f40909b145d097e11f05
UlugbekNG <andynestrov@yandex.com>
2022-09-24 10:44:43 +0500

    added group buttons

--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -1,72 +1,91 @@
 import * as React from "react";
-import { Button, Grid, Box } from "@mui/material";
+import { Button, Grid, Box, Typography } from "@mui/material";
 
 export default function BasicButtons() {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
+      <Box
+        sx={{
+          flexGrow: 1,
+          backgroundColor: "#323232",
+          minHeight: "100vh",
+          padding: 1,
+        }}
+      >
         <Grid container spacing={1}>
-          <Grid item xs={6} md={4} lg={3}>
+          <Grid item xs={6} sm={6} md={4} lg={3}>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{
-                color: "rgb(144, 202, 249)",
-                border: "1px solid rgb(144, 202, 249)",
+                color: "#fff",
+                backgroundColor: "rgb(80 80 80/ 33%)",
+                boxShadow: 0,
+                padding: "15% 40%",
               }}
             >
               301
             </Button>
           </Grid>
-          <Grid item xs={6} md={4} lg={3}>
+          <Grid item xs={6} sm={6} md={4} lg={3}>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{
-                color: "rgb(144, 202, 249)",
-                border: "1px solid rgb(144, 202, 249)",
+                color: "#fff",
+                backgroundColor: "rgb(80 80 80/ 33%)",
+                boxShadow: 0,
+                padding: "15% 40%",
               }}
             >
               302
             </Button>
           </Grid>
-          <Grid item xs={6} md={4} lg={3}>
+          <Grid item xs={6} sm={6} md={4} lg={3}>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{
-                color: "rgb(144, 202, 249)",
-                border: "1px solid rgb(144, 202, 249)",
+                color: "#fff",
+                backgroundColor: "rgb(80 80 80/ 33%)",
+                boxShadow: 0,
+                padding: "15% 40%",
               }}
             >
               303
             </Button>
           </Grid>
-          <Grid item xs={6} md={4} lg={3}>
+          <Grid item xs={6} sm={6} md={4} lg={3}>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{
-                color: "rgb(144, 202, 249)",
-                border: "1px solid rgb(144, 202, 249)",
+                color: "#fff",
+                backgroundColor: "rgb(80 80 80/ 33%)",
+                boxShadow: 0,
+                padding: "15% 40%",
               }}
             >
               304
             </Button>
           </Grid>
-          <Grid item xs={6} md={4} lg={3}>
+          <Grid item xs={6} sm={6} md={4} lg={3}>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{
-                color: "rgb(144, 202, 249)",
-                border: "1px solid rgb(144, 202, 249)",
+                color: "#fff",
+                backgroundColor: "rgb(80 80 80/ 33%)",
+                boxShadow: 0,
+                padding: "15% 40%",
               }}
             >
               305
             </Button>
           </Grid>
-          <Grid item xs={6} md={4} lg={3}>
+          <Grid item xs={6} sm={6} md={4} lg={3}>
             <Button
-              variant="outlined"
+              variant="contained"
               sx={{
-                color: "rgb(144, 202, 249)",
-                border: "1px solid rgb(144, 202, 249)",
+                color: "#fff",
+                backgroundColor: "rgb(80 80 80/ 33%)",
+                boxShadow: 0,
+                padding: "15% 40%",
               }}
             >
               306
