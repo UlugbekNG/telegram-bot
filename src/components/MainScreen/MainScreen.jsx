e810d20ecb7e3990b83ec667b2d9e23be0fb1987
UlugbekNG <andynestrov@yandex.com>
2022-09-24 11:15:22 +0500

    generating groups

--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { Button, Grid, Box } from "@mui/material";
+import { groups } from "../../data/data";
 
-export default function BasicButtons() {
+export default function MainScreen() {
   return (
     <>
       <Box
@@ -13,84 +14,21 @@ export default function BasicButtons() {
         }}
       >
         <Grid container spacing={1}>
-          <Grid item xs={6} sm={6} md={4} lg={3}>
-            <Button
-              variant="contained"
-              sx={{
-                color: "#fff",
-                backgroundColor: "rgb(80 80 80/ 33%)",
-                boxShadow: 0,
-                padding: "15% 40%",
-              }}
-            >
-              301
-            </Button>
-          </Grid>
-          <Grid item xs={6} sm={6} md={4} lg={3}>
-            <Button
-              variant="contained"
-              sx={{
-                color: "#fff",
-                backgroundColor: "rgb(80 80 80/ 33%)",
-                boxShadow: 0,
-                padding: "15% 40%",
-              }}
-            >
-              302
-            </Button>
-          </Grid>
-          <Grid item xs={6} sm={6} md={4} lg={3}>
-            <Button
-              variant="contained"
-              sx={{
-                color: "#fff",
-                backgroundColor: "rgb(80 80 80/ 33%)",
-                boxShadow: 0,
-                padding: "15% 40%",
-              }}
-            >
-              303
-            </Button>
-          </Grid>
-          <Grid item xs={6} sm={6} md={4} lg={3}>
-            <Button
-              variant="contained"
-              sx={{
-                color: "#fff",
-                backgroundColor: "rgb(80 80 80/ 33%)",
-                boxShadow: 0,
-                padding: "15% 40%",
-              }}
-            >
-              304
-            </Button>
-          </Grid>
-          <Grid item xs={6} sm={6} md={4} lg={3}>
-            <Button
-              variant="contained"
-              sx={{
-                color: "#fff",
-                backgroundColor: "rgb(80 80 80/ 33%)",
-                boxShadow: 0,
-                padding: "15% 40%",
-              }}
-            >
-              305
-            </Button>
-          </Grid>
-          <Grid item xs={6} sm={6} md={4} lg={3}>
-            <Button
-              variant="contained"
-              sx={{
-                color: "#fff",
-                backgroundColor: "rgb(80 80 80/ 33%)",
-                boxShadow: 0,
-                padding: "15% 40%",
-              }}
-            >
-              306
-            </Button>
-          </Grid>
+          {groups.map((group) => (
+            <Grid key={group.id} item xs={6} sm={6} md={4} lg={3}>
+              <Button
+                variant="contained"
+                sx={{
+                  color: "#fff",
+                  backgroundColor: "rgb(80 80 80/ 33%)",
+                  boxShadow: 0,
+                  padding: "15% 40%",
+                }}
+              >
+                {group.name}
+              </Button>
+            </Grid>
+          ))}
         </Grid>
       </Box>
     </>
---
+++ src/data/data.js
@@ -0,0 +1,108 @@
+export const groups = [
+  {
+    id: 301,
+    name: "301",
+    timetable: [
+      {
+        monday: [
+          {
+            id: 1,
+            time: "13:30 - 14:50",
+            type: 1,
+            name: "Amaliy ekonometrika",
+            teacher: "Shodmonova G",
+            cabinet: "B-01",
+          },
+          {
+            id: 2,
+            time: "15.00 - 16.20",
+            type: 2,
+            name: "Amaliy ekonometrika",
+            teacher: "Shodmonova G",
+            cabinet: "B01",
+          },
+          {
+            id: 3,
+            time: "16.30 - 17.50",
+            type: 1,
+            name: "Biznes rejalashtirish",
+            teacher: "Dusmurotov G",
+            cabinet: "B-01",
+          },
+        ],
+        tuesday: [
+          {
+            id: 1,
+            time: "13:30 - 14:50",
+            type: 1,
+            name: "Amaliy ekonometrika",
+            teacher: "Shodmonova G",
+            cabinet: "B-01",
+          },
+          {
+            id: 2,
+            time: "15.00 - 16.20",
+            type: 2,
+            name: "Amaliy ekonometrika",
+            teacher: "Shodmonova G",
+            cabinet: "B01",
+          },
+          {
+            id: 3,
+            time: "16.30 - 17.50",
+            type: 1,
+            name: "Biznes rejalashtirish",
+            teacher: "Dusmurotov G",
+            cabinet: "B-01",
+          },
+        ],
+        Wednesday: [
+          {
+            id: 1,
+            time: "13:30 - 14:50",
+            type: 1,
+            name: "Amaliy ekonometrika",
+            teacher: "Shodmonova G",
+            cabinet: "B-01",
+          },
+          {
+            id: 2,
+            time: "15.00 - 16.20",
+            type: 2,
+            name: "Amaliy ekonometrika",
+            teacher: "Shodmonova G",
+            cabinet: "B01",
+          },
+          {
+            id: 3,
+            time: "16.30 - 17.50",
+            type: 1,
+            name: "Biznes rejalashtirish",
+            teacher: "Dusmurotov G",
+            cabinet: "B-01",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 302,
+    name: "302",
+  },
+  {
+    id: 303,
+    name: "303",
+  },
+  {
+    id: 304,
+    name: "304",
+  },
+  {
+    id: 305,
+    name: "305",
+  },
+  {
+    id: 306,
+    name: "306",
+  },
+];
